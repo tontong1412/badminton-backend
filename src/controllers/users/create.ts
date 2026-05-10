16afdf6eae22fd5cc193224463f1011e3986a340
Nonnadda <tontongfordev@gmail.com>
2026-05-10 16:40:48 +0300

    remove next function

--- src/controllers/users/create.ts
+++ src/controllers/users/create.ts
@@ -1,5 +1,5 @@
 import argon2 from 'argon2'
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response } from 'express'
 import { Login, LoginResponse, Player, TokenPayload } from '../../type'
 import UserModel from '../../schema/user'
 import PlayerModel from '../../schema/player'
@@ -22,62 +22,57 @@ interface CreateUserPayload extends Login {
 
 const createUser = async(
   req: Request<unknown, unknown, CreateUserPayload, unknown>,
-  res: Response<LoginResponse>,
-  next: NextFunction) => {
+  res: Response<LoginResponse>) => {
   const { email, password } = req.body
 
-  try {
-    const hash = await argon2.hash(password)
-    const user = new UserModel({
-      email,
-      hash,
-    })
-    const savedUser = await user.save()
+  const hash = await argon2.hash(password)
+  const user = new UserModel({
+    email,
+    hash,
+  })
+  const savedUser = await user.save()
 
-    const player = new PlayerModel({
-      officialName: req.body.officialName,
-      displayName: req.body.displayName,
-      dob: req.body.dob,
-      userID: savedUser._id,
-    })
+  const player = new PlayerModel({
+    officialName: req.body.officialName,
+    displayName: req.body.displayName,
+    dob: req.body.dob,
+    userID: savedUser._id,
+  })
 
-    const savedPlayer = await player.save()
+  const savedPlayer = await player.save()
 
-    await UserModel.findByIdAndUpdate(savedUser._id, { playerID: savedPlayer._id })
+  await UserModel.findByIdAndUpdate(savedUser._id, { playerID: savedPlayer._id })
 
-    const userPayload: TokenPayload =  {
-      id: savedUser._id as Types.ObjectId,
-      email: savedUser.email,
-      playerID: savedPlayer._id as Types.ObjectId,
-    }
-
-    const accessToken = tokenUtils.create(userPayload, config.ACCESS_SECRET, constants.TOKEN.EXPIRE_TIME.ACCESS)
-    const refreshToken = tokenUtils.create(userPayload, config.REFRESH_SECRET, constants.TOKEN.EXPIRE_TIME.REFRESH)
-    await tokenUtils.storeRefreshToken(user._id as string, refreshToken)
-
-    res.cookie('access', accessToken, {
-      httpOnly: true,
-      secure: config.NODE_ENV === 'production',
-      maxAge: config.NODE_ENV === 'production' ? constants.TOKEN.EXPIRE_TIME.ACCESS : 30 * 60 * 1000, // 30 minute in development,
-      sameSite: config.NODE_ENV === 'production' ? 'none' : 'strict'
-    })
-
-    res.cookie('refresh', refreshToken, {
-      httpOnly: true,
-      secure: config.NODE_ENV === 'production',
-      maxAge: config.NODE_ENV === 'production' ? constants.TOKEN.EXPIRE_TIME.REFRESH : 30 * 60 * 1000, // 30 minute in development,
-      sameSite: config.NODE_ENV === 'production' ? 'none' : 'strict'
-    })
-
-    res.json({
-      accessToken,
-      refreshToken,
-      user: userPayload,
-      player: savedPlayer ? savedPlayer.toJSON() as Player : null,
-    })
-  } catch(error: unknown){
-    next(error)
+  const userPayload: TokenPayload =  {
+    id: savedUser._id as Types.ObjectId,
+    email: savedUser.email,
+    playerID: savedPlayer._id as Types.ObjectId,
   }
+
+  const accessToken = tokenUtils.create(userPayload, config.ACCESS_SECRET, constants.TOKEN.EXPIRE_TIME.ACCESS)
+  const refreshToken = tokenUtils.create(userPayload, config.REFRESH_SECRET, constants.TOKEN.EXPIRE_TIME.REFRESH)
+  await tokenUtils.storeRefreshToken(user._id as string, refreshToken)
+
+  res.cookie('access', accessToken, {
+    httpOnly: true,
+    secure: config.NODE_ENV === 'production',
+    maxAge: config.NODE_ENV === 'production' ? constants.TOKEN.EXPIRE_TIME.ACCESS : 30 * 60 * 1000, // 30 minute in development,
+    sameSite: config.NODE_ENV === 'production' ? 'none' : 'strict'
+  })
+
+  res.cookie('refresh', refreshToken, {
+    httpOnly: true,
+    secure: config.NODE_ENV === 'production',
+    maxAge: config.NODE_ENV === 'production' ? constants.TOKEN.EXPIRE_TIME.REFRESH : 30 * 60 * 1000, // 30 minute in development,
+    sameSite: config.NODE_ENV === 'production' ? 'none' : 'strict'
+  })
+
+  res.json({
+    accessToken,
+    refreshToken,
+    user: userPayload,
+    player: savedPlayer ? savedPlayer.toJSON() as Player : null,
+  })
 }
 
 export default createUser
