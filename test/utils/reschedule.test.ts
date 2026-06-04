8375cb247b5a3b818e21cb3a970f9951bee8ed4d
Nonnadda <tontongfordev@gmail.com>
2026-06-05 02:21:29 +0300

    fix error

--- test/utils/reschedule.test.ts
+++ test/utils/reschedule.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-argument, @typescript-eslint/unbound-method */
 import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest'
 import { Types } from 'mongoose'
 import { Response } from 'express'
